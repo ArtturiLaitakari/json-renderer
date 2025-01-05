31f5681e7b203adb20e07745e86cf4ae85205cad
ArtturiLaitakari <artturi.laitakari@gmail.com>
2025-01-05 22:17:07 +0200

    supports all material elements

--- src/renderHTML.tsx
+++ src/renderHTML.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
+import renderMaterialUI from './renderMaterialUI.tsx';
 
 const renderHTML = (element) => {
   const { type, props, children } = element;
-  return createElement(type, { ...props, key: props.key }, children);
+
+  if (isMaterialUIComponent(type)) {
+    return renderMaterialUI(element);
+  }
+
+  return createElement(type, props, children);
 };
 
-const ensureProps = (child, index) => { 
-	if (typeof child === 'string') {
-	 return child; 
-	} 
-	if (!child.props) { 
-		child.props = {}; 
-	} 
-	child.props.key = index; 
-	return child; 
+const isMaterialUIComponent = (type) => {
+  if (!type) {
+    return false;
+  }
+  return /^[A-Z]/.test(type);
 };
 
+const createChildren = (children, renderHTML) => {
+  return children.map((child, index) => {
+    const newChild = ensureProps(child, index);
+    return typeof newChild === 'string' ? newChild : renderHTML(newChild);
+  });
+};
 
-const createChildren = (children, renderHTML) => { 
-  return children.map((child, index) => { 
-  	const newChild = ensureProps(child, index); 
-  	return typeof newChild === 'string' ? newChild : 
-  	renderHTML(newChild); 
-  }); 
+const ensureProps = (child, index) => {
+  if (typeof child === 'string') {
+    return child;
+  }
+  if (!child.props) {
+    child.props = {};
+  }
+  child.props.key = index;
+  return child;
 };
 
 const createElement = (type, props, children) => {
-  return React.createElement(type, props, children 
-  	&& createChildren(children, renderHTML));
+	if (!type) {
+    return;
+  }
+  return React.createElement(type, props, children && createChildren(children, renderHTML));
 };
 
 export default renderHTML;
