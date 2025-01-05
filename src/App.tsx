890dba07a2fb0af179dfd71892634f043d70e1e9
ArtturiLaitakari <artturi.laitakari@gmail.com>
2025-01-05 16:58:02 +0200

    Update app.tsx with parameter
    ladataan parametri, oletus on home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import renderHTML from './renderHTML.tsx';
 
 function App() { 
   const [pageData, setPageData] = useState(null); 
+  const [page, setPage] = useState('');
 
   useEffect(() => { 
-    fetch('/pageData.json')
+    const hash = window.location.hash.substring(1) || 'home'; 
+    const page = `/${hash}.json`;
+
+    setPage(hash);
+    fetch(page)
       .then((response) => response.json())
       .then((data) => setPageData(data))
       .catch((error) => console.error('Error fetching data:', error)); 
